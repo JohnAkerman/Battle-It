48867658e34ffbe8eb21f4be1eab89c72951e814
John Akerman <john.c.akerman@gmail.com>
2016-12-11 22:16:57 +0000

    Added player turret with mouse aiming

--- player.js
+++ player.js
@@ -1,10 +1,19 @@
 function Player() {
-    this.pos = createVector(width/2, height/2);
+    this.pos = createVector(width * .5, height * .5);
     this.vel = createVector(0, 0);
     this.accel = createVector(0,0);
     this.w = 20;
     this.h = 20;
+    this.wHalf = this.w * .5;
+    this.hHalf = this.h * .5;
     this.sizeInc = 3;
+
+    this.turrentAngle = createVector(0, 0);
+
+    this.turretW = 5;
+    this.turretH = 25;
+    this.turretWHalf = this.turretW / 2;
+    this.turretHHalf = this.turretH / 2;
 
     this.update = function() {
         this.vel.add(this.accel);
@@ -12,12 +21,26 @@ function Player() {
         this.accel.mult(0);
 
         this.checkBounds();
+
+        this.turrentAngle = Math.atan2(mouseY - this.pos.y, mouseX - this.pos.x) - 1.5708; // 90 deg
     }
 
     this.render = function() {
         noStroke();
         fill(0,0,255);
         rect(this.pos.x, this.pos.y, this.w, this.h);
+
+        stroke(255,0,0);
+        line(this.pos.x + this.hHalf, this.pos.y + this.wHalf, mouseX, mouseY)
+
+        noStroke();
+        push();
+        translate(this.pos.x + this.hHalf, this.pos.y + this.wHalf);
+        rotate(this.turrentAngle);
+        fill(255,255,255);
+        rect(-this.turretWHalf, 0, this.turretW, this.turretH);
+        pop();
+
     }
 
     this.applyForce = function(f) {
